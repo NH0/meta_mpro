0caa6652341ebdd73d70e38d427ca2b8b6f4f0fc
NH0 <thibaultf@protonmail.com>
2020-10-30 17:16:42 +0100

    feat: create quartering

--- main.py
+++ main.py
@@ -34,17 +34,13 @@ class Instance:
             self._data[i] = line
             
         self._data_x = np.array(sorted(self._data.items(), key=lambda x: x[1][0]))
-        # self._data_y = np.array(sorted(self._data.items(), key=lambda x: x[1][1]))
+        self._data_y = np.array(sorted(self._data.items(), key=lambda x: x[1][1]))
         self._n = len(self._data)
     
     def _distance_ind(self,i,j):
         
-        return Instance.distance(self._data[i],self._data[j])
-    
-    @staticmethod
-    def distance(x,y):
-        
-        return np.sqrt((x[0]-y[0])**2 + (x[1]-y[1])**2)
+        return distance(self._data[i],self._data[j])
+
     
 
 class Solution(Instance):
@@ -193,11 +189,56 @@ class Solution(Instance):
         
         plt.show()
 
-        
-            ##
+def distance(x1,x2):
+    
+    return np.sqrt((x1[0]-x2[0])**2 + (x1[1]-x2[1])**2)
+
+def compute_mean(list_targets, axis=0):
+    axis_sum = 0
+    for element in list_targets:
+        axis_sum += element[1][axis]
+
+    return axis_sum / len(element)
+
+def compute_barycenter(list_targets):
+    x_mean = compute_mean(list_targets, 0)
+    y_mean = compute_mean(list_targets, 1)
+
+    return [x_mean, y_mean]
+
+def find_closest_target(list_targets, barycenter):
+    smallest_distance_to_barycenter = distance(list_targets[0][1], barycenter)
+    closest_target = list_targets[0]
+    for target in list_targets[1:]:
+        distance_to_barycenter = distance(target[1], barycenter)
+        if distance_to_barycenter < smallest_distance_to_barycenter:
+            smallest_distance_to_barycenter = distance_to_barycenter
+            closest_target = target
+
+    return closest_target
+
+def find_sensors(solution, list_targets):
+    return []
 
 def quartering(solution, size=20):
-    return 0
+    x_minimum = solution._data_x[0][1][0]
+    x_maximum = solution._data_x[-1][1][0]
+    y_minimum = solution._data_y[0][1][1]
+    y_maximum = solution._data_y[-1][1][1]
+
+    length = int(x_maximum - x_minimum) + 1
+    width = int(y_maximum - y_minimum) + 1
+
+    length_quarter = length * size / 100
+    width_quarter = width * size / 100
+
+    quarters = [[[] for j in range(width)] for i in range(length)]
+
+    for target in solution._data.items():
+        quarters[int((target[1][0] - x_minimum) / length_quarter)]\
+            [int((target[1][1] - y_minimum) / width_quarter)].append(target)
+
+    
         
 
 Solution1 = Solution(NAME)
@@ -213,7 +254,8 @@ Solution1.optimize_locally()
 t3 = time()
 print(Solution1.score())
 print(t3-t2)
-Solution1.plot_sensors()
+quartering(Solution1)
+# Solution1.plot_sensors()
 
 # compteur = 0
 # score = 0
